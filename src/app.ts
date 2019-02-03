b0b316f29f458e3405d24680181c03a4d8ec5402
jerrynim <tjerry3@naver.com>
2019-02-03 21:08:44 +0900

    init

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import cors from "cors";
+import { GraphQLServer } from "graphql-yoga";
+import helmet from "helmet";
+import logger from "morgan";
+
+class App {
+  public app: GraphQLServer;
+  constructor() {
+    this.app = new GraphQLServer({});
+    this.middlewares();
+  }
+  private middlewares = (): void => {
+    this.app.express.use(cors());
+    this.app.express.use(helmet());
+    /* 요청을 안전한지 검사*/
+    this.app.express.use(logger("dev"));
+    /*미들웨어가 로그를 남김*/
+  };
+}
+export default new App().app;
