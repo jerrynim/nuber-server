1504b85477e487e168bf33badc76f2da1c14a9b8
jerrynim <tjerry3@naver.com>
2019-02-05 16:00:48 +0900

    User Entity

---
+++ src/entities/User.ts
@@ -0,0 +1,70 @@
+import { IsEmail } from "class-validator";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: "text", unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column({ type: "boolean", default: false })
+  verifiedEmail: boolean;
+
+  @Column({ type: "text" })
+  firstName: string;
+
+  @Column({ type: "text" })
+  lastName: string;
+
+  @Column({ type: "int" })
+  age: number;
+
+  @Column({ type: "text" })
+  password: string;
+
+  @Column({ type: "text" })
+  phoneNumber: string;
+
+  @Column({ type: "boolean", default: false })
+  verifiedPhonenNumber: boolean;
+
+  @Column({ type: "text" })
+  profilePhoto: string;
+
+  @Column({ type: "boolean", default: false })
+  isDriving: boolean;
+
+  @Column({ type: "boolean", default: false })
+  isRiding: boolean;
+
+  @Column({ type: "boolean", default: false })
+  isTaken: boolean;
+
+  @Column({ type: "double precision", default: 0 })
+  lastLng: number;
+
+  @Column({ type: "double precision", default: 0 })
+  lastLat: number;
+
+  @Column({ type: "double precision", default: 0 })
+  lastOrientation: number;
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  @CreateDateColumn()
+  createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
+}
+
+export default User;
