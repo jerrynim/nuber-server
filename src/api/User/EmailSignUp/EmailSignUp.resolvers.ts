d1eb3b33f37c7e9ec9df2cf15363118a0063831d
jerrynim <tjerry3@naver.com>
2019-02-09 15:33:51 +0900

    EmailSending while EmailSignUp

--- src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -1,18 +1,21 @@
 import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
 import {
-  EmailSignInMutationArgs,
-  EmailSignInResponse
+  EmailSignUpMutationArgs,
+  EmailSignUpResponse
 } from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
 import createJWT from "../../../utils/createJWT";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
 
 const resolvers: Resolvers = {
   Mutation: {
     EmailSignUp: async (
       _,
-      args: EmailSignInMutationArgs
-    ): Promise<EmailSignInResponse> => {
+      args: EmailSignUpMutationArgs
+    ): Promise<EmailSignUpResponse> => {
       const { email } = args;
+      /*User가 존재하는지확인 >> 휴대폰인증이 되어있는지 확인>> 유저생성 >> 이메일전송 */
       try {
         const existingUser = await User.findOne({ email });
         if (existingUser) {
@@ -22,13 +25,35 @@ const resolvers: Resolvers = {
             token: null
           };
         } else {
-          const newUser = await User.create({ ...args }).save();
-          const token = createJWT(newUser.id);
-          return {
-            ok: true,
-            error: null,
-            token
-          };
+          const phoneVerification = await Verification.findOne({
+            payload: args.phoneNumber,
+            verified: true
+          });
+          if (phoneVerification) {
+            const newUser = await User.create({ ...args }).save();
+            if (newUser.email) {
+              const emailVerification = await Verification.create({
+                payload: newUser.email,
+                target: "EMAIL"
+              }).save();
+              await sendVerificationEmail(
+                newUser.fullName,
+                emailVerification.key
+              );
+            }
+            const token = createJWT(newUser.id);
+            return {
+              ok: true,
+              error: null,
+              token
+            };
+          } else {
+            return {
+              ok: false,
+              error: "You haven't verified your phone nuber",
+              token: null
+            };
+          }
         }
       } catch (error) {
         return {
---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,22 @@
+import Mailgun from "mailgun-js";
+
+const mailGunClient = new Mailgun({
+  apiKey: process.env.MAILGUN_API_KEY || "",
+  domain: "sandbox12824214b7b540b6b44945a82882ccab.mailgun.org"
+});
+
+const sendEmail = (subject: string, html: string) => {
+  const emailData = {
+    from: "tjerry3@naver.com",
+    to: "tjerry3@naver.com",
+    subject,
+    html
+  };
+  return mailGunClient.messages().send(emailData);
+};
+
+export const sendVerificationEmail = (fullName: string, key: string) => {
+  const emailSubject = `Hello! ${fullName}, please verify your email`;
+  const emailBody = `verify your email by clicking <a href="http://nuber.com/verification/${key}/">here</a>`;
+  return sendEmail(emailSubject, emailBody);
+};
