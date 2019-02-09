12d8d05f64706b6c64c1ce86189860faa850912e
jerrynim <tjerry3@naver.com>
2019-02-09 15:45:00 +0900

    Send Email verification resolver

---
+++ src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
@@ -0,0 +1,48 @@
+import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
+import { RequestEmailVerificationResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
+
+/*이메일이 있다면 기존 Verifcatino 이 있으면 삭제 새로운 Verification발송 */
+const resolvers: Resolvers = {
+  Mutation: {
+    RequestEmailVerification: privateResolver(
+      async (_, __, { req }): Promise<RequestEmailVerificationResponse> => {
+        const user: User = req.user;
+        if (user.email) {
+          try {
+            const oldVerification = await Verification.findOne({
+              payload: user.email
+            });
+            if (oldVerification) {
+              oldVerification.remove();
+            }
+            const newVerification = await Verification.create({
+              payload: user.email,
+              target: "EMAIL"
+            }).save();
+            await sendVerificationEmail(user.fullName, newVerification.key);
+            return {
+              ok: true,
+              error: null
+            };
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "Your user has no email to verify"
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
