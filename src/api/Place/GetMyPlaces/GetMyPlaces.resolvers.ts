9d825b0773584a84f633ce87c5e6c487748f51c5
jerrynim <tjerry3@naver.com>
2019-02-10 14:09:53 +0900

    GeyMyPlace

---
+++ src/api/Place/GetMyPlaces/GetMyPlaces.resolvers.ts
@@ -0,0 +1,37 @@
+import User from "../../../entities/User";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetMyPlaces: privateResolver(async (_, __, { req }) => {
+      try {
+        const user = await User.findOne(
+          { id: req.user.id },
+          { relations: ["places"] }
+        );
+        /* DataBase에서 불러오는 방식 */
+        if (user) {
+          return {
+            ok: true,
+            places: user.places,
+            error: null
+          };
+        } else {
+          return {
+            ok: false,
+            places: null,
+            error: "User not found"
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          places: null
+        };
+      }
+    })
+  }
+};
+export default resolvers;
